46d6e2be11e2af0f76f59a9eb7c3c31db1d0f26d
Dư Nguyễn <dunguyen@Dus-MacBook-Air.local>
2025-08-18 16:07:06 +0700

    feat: added api interceptor

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,16 +1,19 @@
 import {
   ApplicationConfig,
   provideBrowserGlobalErrorListeners,
-  provideZoneChangeDetection
+  provideZoneChangeDetection,
 } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
+import { apiInterceptor } from './core/interceptors/api.interceptor';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(routes)
-  ]
+    provideRouter(routes),
+    provideHttpClient(withInterceptors([apiInterceptor])),
+  ],
 };
