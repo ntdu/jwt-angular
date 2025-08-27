0993b1880d0bb4c6512a213a0020fead834da636
Dư Nguyễn <dunguyen@Dus-MacBook-Air.local>
2025-08-27 18:12:58 +0700

    feat: added environemnt folder

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,6 @@
+export const environment = {
+  production: false,
+  apiUrl: 'https://your-production-api.com/api/v1',
+  googleClientId: '636991481799-63etd2mfpue9qkp3joke7g20p4r9fvs1.apps.googleusercontent.com',
+  appName: 'JWT Angular',
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,6 @@
+export const environment = {
+  production: false,
+  apiUrl: 'http://localhost:8000/api/v1',
+  googleClientId: '636991481799-63etd2mfpue9qkp3joke7g20p4r9fvs1.apps.googleusercontent.com',
+  appName: 'JWT Angular Demo',
+};
