eedd767aaa7f72309117f37354a7afc9614586f0
Dư Nguyễn <dunguyen@Dus-MacBook-Air.local>
2025-08-28 23:45:46 +0700

    feat: added error global handler

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -8,12 +8,13 @@ import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { apiInterceptor } from './core/interceptors/api.interceptor';
+import { errorInterceptor } from './core/interceptors/error.interceptor';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
-    provideHttpClient(withInterceptors([apiInterceptor])),
+    provideHttpClient(withInterceptors([apiInterceptor, errorInterceptor])),
   ],
 };
